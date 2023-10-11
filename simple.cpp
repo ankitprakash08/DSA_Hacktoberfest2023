30b11cce1ecf71a2def7e2547d6fa199fb5616d8
ANKIT PRAKASH <88717899+ankitprakash08@users.noreply.github.com>
2023-10-11 18:57:51 +0530

    Create simple.cpp

---
+++ simple.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+
+int main() {
+    std::stack<int> myStack;
+
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+
+    if (myStack.empty()) {
+        std::cout << "Stack is empty." << std::endl;
+    } else {
+        std::cout << "Stack is not empty." << std::endl;
+    }
+
+    while (!myStack.empty()) {
+        std::cout << "Popped: " << myStack.top() << std::endl;
+        myStack.pop();
+    }
+
+    if (myStack.empty()) {
+        std::cout << "Stack is empty." << std::endl;
+    } else {
+        std::cout << "Stack is not empty." << std::endl;
+    }
+
+    return 0;
+}
